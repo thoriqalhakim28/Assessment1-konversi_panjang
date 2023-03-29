c31ce88c63c67fe7006f228b47617c2436c78c7a
Thoriq Al Hakim <tah280103@gmail.com>
2023-03-29 10:01:42 +0700

    Membuat isi dari tampilan menu (isi Dropdown)

--- app/src/main/java/org/d3if3139/assessment1/MainActivity.kt
+++ app/src/main/java/org/d3if3139/assessment1/MainActivity.kt
@@ -2,10 +2,29 @@ package org.d3if3139.assessment1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
+import org.d3if3139.assessment1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    lateinit var konversiDari : AutoCompleteTextView
+    lateinit var konversiKe : AutoCompleteTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        konversiDari = binding.dari
+        konversiKe = binding.ke
+
+        val satuan_panjang = resources.getStringArray(R.array.list_panjang)
+        val adapter = ArrayAdapter(this, R.layout.list_panjang, satuan_panjang)
+
+        konversiDari.setAdapter(adapter)
+        konversiKe.setAdapter(adapter)
     }
 }
