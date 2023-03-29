9c594f2e023c39990ca935536b290e4d97b5452c
Thoriq Al Hakim <tah280103@gmail.com>
2023-03-29 13:02:35 +0700

    Menghitung nilai konversi

--- app/src/main/java/org/d3if3139/assessment1/MainActivity.kt
+++ app/src/main/java/org/d3if3139/assessment1/MainActivity.kt
@@ -6,6 +6,7 @@ import android.text.TextUtils
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import org.d3if3139.assessment1.databinding.ActivityMainBinding
 
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var inputPanjang : com.google.android.material.textfield.TextInputEditText
     lateinit var btnKonversi : Button
     lateinit var btnReset : Button
+    lateinit var hasilKonversi : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,8 +30,9 @@ class MainActivity : AppCompatActivity() {
         btnKonversi = binding.btnKonveri
         btnReset = binding.btnReset
         inputPanjang = binding.inputPanjang
+        hasilKonversi = binding.hasil
 
-        val satuan_panjang = resources.getStringArray(R.array.list_panjang)
+        val satuan_panjang = resources.getStringArray(R.array.satuan_panjang)
         val adapter = ArrayAdapter(this, R.layout.list_panjang, satuan_panjang)
 
         konversiDari.setAdapter(adapter)
@@ -61,6 +64,63 @@ class MainActivity : AppCompatActivity() {
             jikaSama = true
             Toast.makeText(this, "Pilih satuan panjang yang berbeda", Toast.LENGTH_SHORT).show()
         }
+
+        val nilaiPanjang = nilai.toFloat()
+        var hasil = 0f
+
+        when(dari+ke) {
+
+            // km -> m
+            resources.getStringArray(R.array.satuan_panjang)[0] + resources.getStringArray(R.array.satuan_panjang)[1] -> {
+                hasil = nilaiPanjang * 1000f
+            }
+            // km -> cm
+            resources.getStringArray(R.array.satuan_panjang)[0] + resources.getStringArray(R.array.satuan_panjang)[2] -> {
+                hasil = nilaiPanjang * 100000f
+            }
+            // km -> mm
+            resources.getStringArray(R.array.satuan_panjang)[0] + resources.getStringArray(R.array.satuan_panjang)[3] -> {
+                hasil = nilaiPanjang * 1000000f
+            }
+            // m -> km
+            resources.getStringArray(R.array.satuan_panjang)[1] + resources.getStringArray(R.array.satuan_panjang)[0] -> {
+                hasil = nilaiPanjang / 1000f
+            }
+            // m -> cm
+            resources.getStringArray(R.array.satuan_panjang)[1] + resources.getStringArray(R.array.satuan_panjang)[2] -> {
+                hasil = nilaiPanjang * 100f
+            }
+            // m -> mm
+            resources.getStringArray(R.array.satuan_panjang)[1] + resources.getStringArray(R.array.satuan_panjang)[3] -> {
+                hasil = nilaiPanjang * 1000f
+            }
+            // cm -> km
+            resources.getStringArray(R.array.satuan_panjang)[2] + resources.getStringArray(R.array.satuan_panjang)[0] -> {
+                hasil = nilaiPanjang / 100000f
+            }
+            // cm -> m
+            resources.getStringArray(R.array.satuan_panjang)[2] + resources.getStringArray(R.array.satuan_panjang)[1] -> {
+                hasil = nilaiPanjang / 100f
+            }
+            // cm -> mm
+            resources.getStringArray(R.array.satuan_panjang)[2] + resources.getStringArray(R.array.satuan_panjang)[3] -> {
+                hasil = nilaiPanjang * 10f
+            }
+            // mm -> km
+            resources.getStringArray(R.array.satuan_panjang)[3] + resources.getStringArray(R.array.satuan_panjang)[0] -> {
+                hasil = nilaiPanjang / 1000000f
+            }
+            // mm -> m
+            resources.getStringArray(R.array.satuan_panjang)[3] + resources.getStringArray(R.array.satuan_panjang)[1] -> {
+                hasil = nilaiPanjang / 1000f
+            }
+            // mm -> cm
+            resources.getStringArray(R.array.satuan_panjang)[3] + resources.getStringArray(R.array.satuan_panjang)[2] -> {
+                hasil = nilaiPanjang / 10f
+            }
+        }
+
+        hasilKonversi.text = hasil.toString()
     }
 
 }
